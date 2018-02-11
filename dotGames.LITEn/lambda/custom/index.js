3bda28608f859c01bb7b08d612621d7dd7634b15
daltonhahn <hahnd11@gmail.com>
2018-02-10 19:31:33 -0600

    Working on skill

--- dotGames.LITEn/lambda/custom/index.js
+++ dotGames.LITEn/lambda/custom/index.js
@@ -1,6 +1,7 @@
 "use strict";
 exports.__esModule = true;
 var Alexa = require("alexa-sdk");
+/*         let token = this.event.session.user.accessToken; */
 var handlers = {
     'LaunchRequest': function () {
         this.emit('AMAZON.HelpIntent');
@@ -8,6 +9,20 @@ var handlers = {
     'SessionEndedRequest': function () {
         var request = this.event.request;
         console.log('Session ended with reason: ' + request.reason);
+    },
+    'Continue': function () {
+        this.response.speak('Testing mode, accessing lighten');
+        this.emit(':responseReady');
+    },
+    'Play': function () {
+        var request = this.event.request;
+        var color = request.intent.slots["color"].value;
+        this.response.speak("Color is garbage");
+        this.emit(':resopnseReady');
+    },
+    'CreateGame': function () {
+        this.response.speak('Creating game');
+        this.emit(':responseReady');
     },
     'AMAZON.StopIntent': function () {
         this.response.speak('Bye');
@@ -23,6 +38,7 @@ var handlers = {
     },
     'Unhandled': function () {
         this.response.speak("Sorry, I didn't get that.");
+        this.emit(':responseReady');
     }
 };
 function handler(event, context) {
--- dotGames.LITEn/lambda/custom/index.ts
+++ dotGames.LITEn/lambda/custom/index.ts
@@ -1,6 +1,9 @@
 import * as Alexa from "alexa-sdk";
 import * as Request from "request";
 
+/*         let token = this.event.session.user.accessToken; */
+
+
 let handlers: Alexa.Handlers<Alexa.Request> = {
     'LaunchRequest': function () {
         this.emit('AMAZON.HelpIntent');
@@ -8,6 +11,20 @@ let handlers: Alexa.Handlers<Alexa.Request> = {
     'SessionEndedRequest' : function() {
         let request = this.event.request as Alexa.SessionEndedRequest;
         console.log('Session ended with reason: ' + request.reason);
+    },
+    'Continue' : function() {
+        this.response.speak('Testing mode, accessing lighten');
+        this.emit(':responseReady');
+    },
+    'Play' : function() {
+      let request = this.event.request as Alexa.IntentRequest;
+      let color = request.intent.slots["color"].value;
+      this.response.speak("Color is garbage");
+      this.emit(':resopnseReady');
+    },
+    'CreateGame' : function() {
+        this.response.speak('Creating game');
+        this.emit(':responseReady');
     },
     'AMAZON.StopIntent' : function() {
         this.response.speak('Bye');
@@ -23,6 +40,7 @@ let handlers: Alexa.Handlers<Alexa.Request> = {
     },
     'Unhandled' : function() {
         this.response.speak("Sorry, I didn't get that.");
+        this.emit(':responseReady');
     }
 };
 
